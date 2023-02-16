f61a0593883eff399bc5987591ac0ab927c29d61
Asahi Cantu <asahi.cantu@capgemini.com>
2023-02-16 11:15:00 +0100

    Added source partsunlimited

---
+++ src/PartsUnlimited/PartsUnlimited.Shared/TaxService.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsUnlimited.Shared
+{
+    public class TaxService
+{
+    static public decimal CalculateTax(decimal taxable, string postalCode)
+    {
+        return taxable * (decimal).1;
+    }
+}
+}
